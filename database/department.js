0a987b86fc80b5ca587c713e7479f6681c1d3e53
mmoghal <mmoghal29@gmail.com>
2023-05-13 15:33:32 -0400

    Created department.js

---
+++ database/department.js
@@ -0,0 +1,34 @@
+const connection = require('../connection');
+
+// Function to get all departments
+const getAllDepartments = () => {
+  return new Promise((resolve, reject) => {
+    const query = 'SELECT * FROM department';
+    connection.query(query, (err, departments) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(departments);
+    });
+  });
+};
+
+// Function to add a new department
+const addDepartment = (department) => {
+  return new Promise((resolve, reject) => {
+    const query = 'INSERT INTO department SET ?';
+    connection.query(query, department, (err, result) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(result.insertId);
+    });
+  });
+};
+
+module.exports = {
+  getAllDepartments,
+  addDepartment
+};
