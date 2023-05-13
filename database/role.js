460b0c122e20532eb1c52e8488f59037a46609cf
mmoghal <mmoghal29@gmail.com>
2023-05-13 15:43:38 -0400

    Added role.js Added Function to get all roles Added Function to add a new role

---
+++ database/role.js
@@ -0,0 +1,38 @@
+const connection = require('./connection');
+
+// Function to get all roles
+const getAllRoles = () => {
+  return new Promise((resolve, reject) => {
+    const query = `
+      SELECT role.id, role.title, role.salary, department.name AS department
+      FROM role
+      INNER JOIN department ON role.department_id = department.id
+    `;
+    connection.query(query, (err, roles) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(roles);
+    });
+  });
+};
+
+// Function to add a new role
+const addRole = (role) => {
+  return new Promise((resolve, reject) => {
+    const query = 'INSERT INTO role SET ?';
+    connection.query(query, role, (err, result) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(result.insertId);
+    });
+  });
+};
+
+module.exports = {
+  getAllRoles,
+  addRole
+};
