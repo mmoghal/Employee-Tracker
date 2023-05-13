86a75a04942a0a4fb8f7824b8d9c2d558740248d
mmoghal <mmoghal29@gmail.com>
2023-05-13 14:47:03 -0400

    Initial commit with .env file Created connection.js Installed MySQL2, Inquirer and console.table package

---
+++ connection.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const mysql = require('mysql2');
+
+// Create a connection to the MySQL database
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
+
+// Connect to the MySQL database
+connection.connect(function (err) {
+  if (err) {
+    console.error('Error connecting to the database: ' + err.stack);
+    return;
+  }
+  console.log('Connected to the database.');
+});
+
+// Export the connection
+module.exports = connection;
